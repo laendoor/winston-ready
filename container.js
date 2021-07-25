a970d1131c4199888e77b1686b85ada8c9a7cbc7
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-07-25 10:24:28 -0300

    :recycle: add an option to disable creating default log files

--- container.js
+++ container.js
@@ -14,24 +14,24 @@ const LOG_CONSOLE_LEVEL = process.env.LOG_CONSOLE_LEVEL || 'debug';
 
 // ***** Transports *****
 
-const logTransport = new winston.transports.File({
+const logTransport = () => new winston.transports.File({
   level: LOG_LEVEL,
   filename: `${LOG_PATH}/${LOG_NAME}_${LOG_LEVEL}.log`,
 });
 
-const errorTransport = new winston.transports.File({
+const errorTransport = () => new winston.transports.File({
   level: 'error',
   filename: `${LOG_PATH}/${LOG_NAME}_error.log`,
 });
 
-const dailyRotateTransport = new winston.transports.DailyRotateFile({
+const dailyRotateTransport = () => new winston.transports.DailyRotateFile({
   dirname: LOG_PATH,
   filename: `${LOG_NAME}_%DATE%.log`,
   datePattern: LOG_DATE_PATTERN,
   maxFiles: LOG_DAYS,
 });
 
-const consoleTransport = new winston.transports.Console({
+const consoleTransport = () => new winston.transports.Console({
   level: LOG_CONSOLE_LEVEL,
   handleExceptions: true,
   format: winston.format.combine(
@@ -50,37 +50,33 @@ const container = new winston.Container();
 
 const transports = [];
 
-if (LOG_STYLE === 'single') {
-  transports.push(logTransport);
-  transports.push(errorTransport);
+if (LOG_STYLE === 'single' || LOG_STYLE === 'both') {
+  transports.push(logTransport());
+  transports.push(errorTransport());
 }
 
-if (LOG_STYLE === 'rotate') {
-  transports.push(dailyRotateTransport);
-}
-
-if (LOG_STYLE === 'both') {
-  transports.push(logTransport);
-  transports.push(errorTransport);
-  transports.push(dailyRotateTransport);
+if (LOG_STYLE === 'rotate' || LOG_STYLE === 'both') {
+  transports.push(dailyRotateTransport());
 }
 
 if (process.env.NODE_ENV === 'development' || LOG_CONSOLE === 'on') {
-  transports.push(consoleTransport);
+  transports.push(consoleTransport());
 }
 
-container.add('default', {
-  level: LOG_LEVEL,
-  format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.errors({ stack: true }),
-    winston.format.splat(),
-    winston.format.json(),
-  ),
-  defaultMeta: { service: LOG_NAME },
-  transports,
-  exitOnError: false,
-});
+if (transports.length > 0) {
+  container.add('default', {
+    level: LOG_LEVEL,
+    format: winston.format.combine(
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      winston.format.errors({ stack: true }),
+      winston.format.splat(),
+      winston.format.json(),
+    ),
+    defaultMeta: { service: LOG_NAME },
+    transports,
+    exitOnError: false,
+  });
+}
 
 module.exports = {
   container,
