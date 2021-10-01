597739ee87d25cdd2de2392d3fb5b7d547c3a7e5
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-10-01 12:05:07 -0300

    :recycle: :label: fix types

--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,12 @@
-import { Container, Logger } from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
-import { ConsoleTransportInstance, FileTransportInstance } from "winston/lib/winston/transports";
-
 /**
  * Transport for Logging based con level definition
  */
-declare function logTransport(): FileTransportInstance;
+declare function logTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging error only
  */
-declare function errorTransport(): FileTransportInstance;
+declare function errorTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging based con level definition
@@ -20,9 +16,9 @@ declare function dailyRotateTransport(): DailyRotateFile;
 /**
  * Transport for Logging based con level definition
  */
-declare function consoleTransport(): ConsoleTransportInstance;
+declare function consoleTransport(): winston.transports.ConsoleTransportInstance;
 
-declare const container: Container;
+declare const container: winston.Container;
 
 declare const transports: winston.transports.Transport[];
 
