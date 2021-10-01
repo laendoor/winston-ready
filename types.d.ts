1152953c3548c98bd85d82d5e9c2f21357c7daae
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-10-01 12:22:43 -0300

    :recycle: :label: fix module exportation

--- types.d.ts
+++ types.d.ts
@@ -1,27 +1,27 @@
 /**
  * Transport for Logging based con level definition
  */
-export declare function logTransport(): winston.transports.FileTransportInstance;
+declare function logTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging error only
  */
-export declare function errorTransport(): winston.transports.FileTransportInstance;
+declare function errorTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging based con level definition
  */
-export declare function dailyRotateTransport(): DailyRotateFile;
+declare function dailyRotateTransport(): DailyRotateFile;
 
 /**
  * Transport for Logging based con level definition
  */
-export declare function consoleTransport(): winston.transports.ConsoleTransportInstance;
-
-export declare const container: winston.Container;
-
-export declare const transports: winston.transports.Transport[];
+declare function consoleTransport(): winston.transports.ConsoleTransportInstance;
 
 declare const logger: Logger;
 
-export default logger;
+declare module 'winston-ready' {
+  export default logger;
+  export declare const container: winston.Container;
+  export declare const transports: winston.transports.Transport[];
+}
