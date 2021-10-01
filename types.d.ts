bef8c9865e480f09e09d780c727b064778eaa4b4
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-10-01 12:17:31 -0300

    :recycle: :label: fix types

--- types.d.ts
+++ types.d.ts
@@ -1,26 +1,27 @@
 /**
  * Transport for Logging based con level definition
  */
-declare function logTransport(): winston.transports.FileTransportInstance;
+export declare function logTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging error only
  */
-declare function errorTransport(): winston.transports.FileTransportInstance;
+export declare function errorTransport(): winston.transports.FileTransportInstance;
 
 /**
  * Transport for Logging based con level definition
  */
-declare function dailyRotateTransport(): DailyRotateFile;
+export declare function dailyRotateTransport(): DailyRotateFile;
 
 /**
  * Transport for Logging based con level definition
  */
-declare function consoleTransport(): winston.transports.ConsoleTransportInstance;
+export declare function consoleTransport(): winston.transports.ConsoleTransportInstance;
 
-declare const container: winston.Container;
+export declare const container: winston.Container;
 
-declare const transports: winston.transports.Transport[];
+export declare const transports: winston.transports.Transport[];
 
 declare const logger: Logger;
 
+export default logger;
